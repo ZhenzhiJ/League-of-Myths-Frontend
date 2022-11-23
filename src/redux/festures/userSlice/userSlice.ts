50d65161ebcc28d6c5201b4b473070d6131de1ca
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-11-23 13:09:21 +0100

    Create userSlice with login and logout

---
+++ src/redux/festures/userSlice/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User, UserState } from "./types";
+
+const initialState: UserState = {
+  username: "",
+  id: "",
+  token: "",
+  isLogged: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (currentUser, action: PayloadAction<User>) => ({
+      ...action.payload,
+      isLogged: true,
+    }),
+
+    logoutUser: (previousUser) => ({
+      ...initialState,
+    }),
+  },
+});
+
+export const {
+  loginUser: loginUserActionCreator,
+  logoutUser: logoutUserActionCreator,
+} = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
