d2b98a1fcdf3c1060d1d6d6134fbc2e02ca040d2
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-11-27 14:51:38 +0100

    Create Header component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import Header from "../Header/Header";
+import RegisterForm from "../RegisterForm/RegisterForm";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Create your own myths</h1>
+        <RegisterForm />
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header/HeaderStyled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  height: 60px;
+  width: 100%;
+  background-color: #111111;
+  display: flex;
+  align-items: center;
+  img {
+    padding-left: 20px;
+  }
+
+  .title {
+    font-size: 16px;
+    color: #bcbd6e;
+    width: 77px;
+  }
+`;
+
+export default HeaderStyled;
