8416cf53dea1be062178b840b76e643d2af93b2f
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-11-29 13:07:20 +0100

    Create login page and test

---
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -0,0 +1,26 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import LoginPage from "./LoginPage";
+
+describe("Given a login page", () => {
+  describe("When it's rendered", () => {
+    test("Then it should show a register form with the label 'username', 'password' and a 'SIGN IN' button", () => {
+      const labelUsername = "Username:";
+      const labelPassword = "Password:";
+      const nameButton = "Login";
+
+      renderWithProviders(<LoginPage />);
+
+      const buttonSignUp = screen.getByRole("button", {
+        name: nameButton,
+      });
+
+      const inputUsername = screen.getByLabelText(labelUsername);
+      const inputPassword = screen.getByLabelText(labelPassword);
+
+      expect(buttonSignUp).toBeInTheDocument();
+      expect(inputUsername).toBeInTheDocument();
+      expect(inputPassword).toBeInTheDocument();
+    });
+  });
+});
