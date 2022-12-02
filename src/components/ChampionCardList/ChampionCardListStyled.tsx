878c19386aedcca3b2c58eb7a71920aa27c5f7a1
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-12-02 18:17:50 +0100

    Add styles to ChampionCardList and ChampionCard

--- src/components/ChampionCardList/ChampionCardListStyled.tsx
+++ src/components/ChampionCardList/ChampionCardListStyled.tsx
@@ -4,6 +4,24 @@ const ChampionCardListStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 20px;
+  .subtitle {
+    color: #bcbd6e;
+    padding-top: 30px;
+    margin: 0;
+    text-align: center;
+    font-size: 48px;
+  }
+  .list-title {
+    margin: 0;
+    font-size: 32px;
+  }
+  .champion-list {
+    display: grid;
+    justify-content: center;
+    gap: 100px;
+    padding: 0px;
+  }
 `;
 
 export default ChampionCardListStyled;
