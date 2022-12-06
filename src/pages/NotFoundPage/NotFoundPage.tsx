654189c4da1ea81b6412786c0ffec9615c878b0f
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-12-06 12:13:19 +0100

    Remove unnecessary import from NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,9 @@
-import CreateForm from "../../components/CreateForm/CreateForm";
 import PageNotFound from "../../components/PageNotFound/PageNotFound";
 
 const NotFoundPage = () => {
   return (
     <main>
       <PageNotFound />
-      <CreateForm />
     </main>
   );
 };
